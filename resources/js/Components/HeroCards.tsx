bbcdd54304aed5b5b137c9e48b9c7172f9b5bb76
pablo-sa10 <pablomoura2016@gmail.com>
2025-02-24 08:37:23 -0300

    aprimoramento dos cards na pagina inicial

--- resources/js/Components/HeroCards.tsx
+++ resources/js/Components/HeroCards.tsx
@@ -8,8 +8,15 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-  } from "@/Components/ui/card"
+} from "@/Components/ui/card"
 import { Check, Linkedin } from "lucide-react";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+
+const benefits = [
+    "4 Team member", 
+    "4 GB Storage", 
+    "Upto 6 pages"
+]
 
 export const HeroCards = () => {
     return (
@@ -18,8 +25,8 @@ export const HeroCards = () => {
             <Card className="absolute w-[340px] -top-[15px] drop-shadow-xl shadow-black/10 dark:shadow-white/10">
                 <CardHeader className="flex flex-row items-center gap-4 pb-2">
                     <Avatar>
-                        <AvatarImage 
-                            alt=""
+                        <AvatarImage
+                            alt="minha foto de perfil"
                             src="https://github.com/pablo-sa10.png"
                         />
                         <AvatarFallback>PM</AvatarFallback>
@@ -36,6 +43,126 @@ export const HeroCards = () => {
 
                 <CardContent>This landing page is awesome!</CardContent>
             </Card>
-        </div>
+
+            {/* TEAM */}
+            <Card className="absolute right-[20px] top-4 w-80 flex flex-col justify-center items-center drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+                <CardHeader className="mt-8 flex justify-center items-center pb-2">
+                    <img
+                        alt="fot do git"
+                        src="https://github.com/pablo-sa10.png"
+                        className="absolute grayscale-[0%] -top-12 rounded-full w-24 aspect-square object-cover"
+                    />
+                    <CardTitle className="text-center">
+                        Pablo Roberto
+                    </CardTitle>
+                    <CardDescription className="font-normal text-primary">
+                        Front Developer
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent className="text-center pb-2">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Veniam, at animi
+                    </p>
+                </CardContent>
+
+                <CardFooter>
+                    <div>
+                        <a
+                            rel="noreferrer"
+                            href="https://github.com/pablo-sa10"
+                            target="_blank"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                size: "sm",
+                            })}
+                        >
+                            <span className="sr-only">Github Icon</span>
+                            <GitHubLogoIcon className="w-5 h-5" />
+                        </a>
+                        <a
+                            rel="noreferrer noopener"
+                            href="https://twitter.com/leo_mirand4"
+                            target="_blank"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                size: "sm",
+                            })}
+                        >
+                            <span className="sr-only">X icon</span>
+                            <svg
+                                role="img"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="fill-foreground w-5 h-5"
+                            >
+                                <title>X</title>
+                                <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+                            </svg>
+                        </a>
+
+                        <a
+                            rel="noreferrer noopener"
+                            href="https://www.linkedin.com/in/leopoldo-miranda/"
+                            target="_blank"
+                            className={buttonVariants({
+                                variant: "ghost",
+                                size: "sm",
+                            })}
+                        >
+                            <span className="sr-only">Linkedin icon</span>
+                            <Linkedin size="20" />
+                        </a>
+
+                    </div>
+                </CardFooter>
+            </Card>
+
+            {/* PRICING */}
+            <Card className="absolute top-[150px] left-[50px] w-72 drop-shadow-xl shadow-black/10 dark:shadow-white/10">
+                <CardHeader>
+                    <CardTitle className="flex items-center justify-between">
+                        Free
+                        <Badge
+                            variant="secondary"
+                            className="text-sm text-primary"
+                        >
+                            Most Popular
+                        </Badge>
+                    </CardTitle>
+                    <div>
+                        <span className="text-3xl font-bold">$0</span>
+                        <span className="text-muted-foreground"> /month</span>
+                    </div>
+
+                    <CardDescription>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    </CardDescription>
+                </CardHeader>
+
+                <CardContent>
+                    <Button className="w-full">Start Free Trial</Button>
+                </CardContent>
+
+                <hr className="w-4/5 m-auto mb-4" />
+
+                <CardFooter className="flex">
+                    <div className="space-y-4">
+                        {benefits.map(
+                            (benefit: string) => (
+                                <li 
+                                    key={benefit}
+                                    className="flex"
+                                >
+                                    <Check className="text-green-500"/>{""}
+                                    <h3 className="ml-2">{benefit}</h3>
+                                </li>
+                            )
+                        )}
+                    </div>
+                </CardFooter>
+            </Card>
+        </div >
     )
 }
--- resources/js/Layouts/GuestLayout.tsx
+++ resources/js/Layouts/GuestLayout.tsx
@@ -7,7 +7,7 @@ import { HeroCards } from '@/Components/HeroCards';
 
 export default function Guest() {
     return (
-        <section className="container px-12 grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
+        <section className="container mx-auto px-12 grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
             <div className="text-center lg:text-start space-y-6">
                 <main className="text-5xl md:text-6xl font-bold">
                     <h1 className="inline">
