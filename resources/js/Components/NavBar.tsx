e654e0abdc49bae17257e195d43bef317931cffe
pablo-sa10 <pablomoura2016@gmail.com>
2025-03-03 08:25:52 -0300

    efeito luz atras dos cards

--- resources/js/Components/NavBar.tsx
+++ resources/js/Components/NavBar.tsx
@@ -44,7 +44,7 @@ const routeList: RouteProps[] = [
 export function NavBar({ auth }: PageProps) {
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  console.log(auth)
+  // console.log(auth)
 
   return (
     <header className="sticky z-10 bg-background/95 supports-[backdrop-filter]:bg-background/60 backdrop-blur top-0 flex shrink-0 items-center gap-2 border-b h-16">
