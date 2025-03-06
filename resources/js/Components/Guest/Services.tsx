ec9f1e48cb378dc68a222be40c2a3fc19a90c726
pablo-sa10 <pablomoura2016@gmail.com>
2025-03-06 16:49:48 -0300

    elaborando services

--- resources/js/Components/Guest/Services.tsx
+++ resources/js/Components/Guest/Services.tsx
@@ -1,5 +1,30 @@
 import { Card, CardDescription, CardHeader, CardTitle } from "@/Components/ui/card";
+import { MagnifierIcon, WalletIcon, ChartIcon } from "@/Components/Icons";
 import cubeLeg from "@images/cube-leg.png";
+
+interface ServiceProps{
+    title: string;
+    description: string;
+    icon: JSX.Element;
+}
+
+const serviceList: ServiceProps[] = [
+    {
+        title: "Code Collaboration",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+        icon: <ChartIcon />
+    },
+    {
+        title: "Code Collaboration",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+        icon: <ChartIcon />
+    },
+    {
+        title: "Code Collaboration",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+        icon: <ChartIcon />
+    },
+]
 
 export const Services = () => {
     return (
