82a92ae5085a38abd00d6aa70c48422e910a5526
pablo-sa10 <pablomoura2016@gmail.com>
2025-03-10 15:33:03 -0300

    tabela de preços feitas

--- resources/js/Components/Guest/Pricing.tsx
+++ resources/js/Components/Guest/Pricing.tsx
@@ -74,7 +74,7 @@ export const Pricing = () => {
     return (
         <section
             id="pricing"
-            className="container py-24 sm:py-32"
+            className="container mx-auto px-5 md:px-24 py-24 sm:py-32"
         >
             <h2 className="text-3xl md:text-4xl font-bold text-center">
                 Get
@@ -92,9 +92,53 @@ export const Pricing = () => {
                 {pricingList.map((pricing: PrincigProps) => (
                     <Card
                         key={pricing.title}
-                        className=""
+                        className={
+                            pricing.popular === PopularPlanType.YES
+                                ? "drop-shadow-xl shadow-black/10 dark:shadow-white/10"
+                                : ""
+                        }
                     >
+                        <CardHeader>
+                            <CardTitle className="flex items-center justify-between">
+                                {pricing.title}
+                                {pricing.popular === PopularPlanType.YES ? (
+                                    <Badge
+                                        variant="secondary"
+                                        className="text-sm text-primary"
+                                    >
+                                        Most Popular
+                                    </Badge>
+                                ) :
+                                    null
+                                }
+                            </CardTitle>
+                            <div>
+                                <span className="text-3xl font-bold">${pricing.price}</span>
+                                <span className="text-muted-foreground"> /month</span>
+                            </div>
 
+                            <CardDescription>{pricing.description}</CardDescription>
+                        </CardHeader>
+
+                        <CardContent>
+                            <Button className="w-full">{pricing.buttonText}</Button>
+                        </CardContent>
+
+                        <hr className="w-4/5 m-auto mb-4" />
+
+                        <CardFooter className="flex">
+                            <div className="space-y-4">
+                                {pricing.benefitList.map((benefit: string) => (
+                                    <span
+                                        key={benefit}
+                                        className="flex"
+                                    >
+                                        <Check className="text-green-500 "/>{" "}
+                                        <h3 className="ml-2">{benefit}</h3>
+                                    </span>
+                                ))}
+                            </div>
+                        </CardFooter>
                     </Card>
                 ))}
             </div>
