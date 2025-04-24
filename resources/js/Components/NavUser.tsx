43c33293e1e5ddd8dc5f1953fc7dc438c0b13151
Pablo <pablomoura2016@gmail.com>
2025-04-23 21:06:51 -0300

    refatorando o componente NavUser para melhorar a legibilidade do código e aprimorar a exibição das informações do usuário no DropdownMenu

--- resources/js/Components/NavUser.tsx
+++ resources/js/Components/NavUser.tsx
@@ -1,69 +1,106 @@
 import { useState } from "react";
-import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from "./ui/sidebar"
+import {
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
+} from "./ui/sidebar";
 import { useAuthContext } from "@/context/AuthUserContext";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
 import { cn } from "@/lib/utils";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import { ChevronsUpDown } from "lucide-react";
 
 type NavUserProps = {
-    isNavBar?: boolean
-    btnClassName?: string
-}
+    isNavBar?: boolean;
+    btnClassName?: string;
+};
 
-export function NavUser({isNavBar, btnClassName}: NavUserProps){
-
+export function NavUser({ isNavBar, btnClassName }: NavUserProps) {
     const { isMobile } = useSidebar();
     const [showLougoutAlert, setShowLougoutAlert] = useState(false);
     const { user } = useAuthContext(); // pega as infos do usuario
 
-    function getUserInitial(name: string): string{
-
+    function getUserInitial(name: string): string {
         const parts = name.trim().split(" ").filter(Boolean);
 
-        if(parts.length === 1){
-            return parts[0].slice(0, 2).toUpperCase()
+        if (parts.length === 1) {
+            return parts[0].slice(0, 2).toUpperCase();
         }
 
-        return parts[0][0].toLocaleUpperCase() + parts[1][0].toLocaleUpperCase() 
+        return (
+            parts[0][0].toLocaleUpperCase() + parts[1][0].toLocaleUpperCase()
+        );
     }
 
-    return(
+    return (
         <SidebarMenu>
             <SidebarMenuItem>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
                         <SidebarMenuButton
-                        size="lg"
-                         className={cn("data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground", btnClassName)}
+                            size="lg"
+                            className={cn(
+                                "data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground",
+                                btnClassName
+                            )}
                         >
                             <Avatar className="h-8 w-8 rounded-lg">
-                                <AvatarFallback className="rounded-lg">{user?.name ? getUserInitial(user.name) : "US"}</AvatarFallback>
+                                <AvatarFallback className="rounded-lg">
+                                    {user?.name
+                                        ? getUserInitial(user.name)
+                                        : "US"}
+                                </AvatarFallback>
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
                                 <span className="truncate font-semibold">
-                                {user?.name}
+                                    {user?.name}
                                 </span>
                                 <span className="truncate text-xs">
-                                {user?.email}
+                                    {user?.email}
                                 </span>
                             </div>
-                            <ChevronsUpDown className="ml-auto size-4"/>
+                            <ChevronsUpDown className="ml-auto size-4" />
                         </SidebarMenuButton>
                     </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-[--radiz-dropdown-menu-trigger-width] min-w-56 rounded-lg" side={isMobile || isNavBar ? "bottom" : "right"}
-                    align="end"
-                    sideOffset={4}
+                    <DropdownMenuContent
+                        className="w-[--radiz-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                        side={isMobile || isNavBar ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
                     >
                         {!isNavBar && (
                             <DropdownMenuLabel>
-
+                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                    <Avatar className="h-8 w-8 rounded-lg">
+                                        <AvatarFallback className="rounded-lg">
+                                            {user?.name
+                                                ? getUserInitial(user.name)
+                                                : "US"}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="truncate font-semibold">
+                                            {user?.name}
+                                        </span>
+                                        <span className="truncate text-xs">
+                                            {user?.email}
+                                        </span>
+                                    </div>
+                                </div>
                             </DropdownMenuLabel>
                         )}
+                        <DropdownMenuSeparator />
                     </DropdownMenuContent>
                 </DropdownMenu>
             </SidebarMenuItem>
         </SidebarMenu>
-    )
+    );
 }
