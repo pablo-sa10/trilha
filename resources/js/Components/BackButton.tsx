56635489db09c2941c6b6405b89469e01173b373
Pablo <pablomoura2016@gmail.com>
2025-05-04 15:02:21 -0300

    feat: refatorar FormCreateLearningPath e adicionar componente BackButton

---
+++ resources/js/Components/BackButton.tsx
@@ -0,0 +1,18 @@
+import { Link } from "@inertiajs/react";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
+import { ArrowLeft } from "lucide-react";
+
+export function BackButton({page, ...props}: {page: string}){
+    return (
+        <Link
+            href={route(page)}
+            {...props}
+            className={cn(buttonVariants(
+                {variant: "destructive", size: "sm"}
+            ))}
+        >
+            <ArrowLeft/>
+        </Link>
+    )
+}
--- resources/js/Components/FormCreateLearningPath.tsx
+++ resources/js/Components/FormCreateLearningPath.tsx
@@ -4,15 +4,6 @@ import { useForm } from "@inertiajs/react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import {
-    Form,
-    FormControl,
-    FormDescription,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
-} from "@/components/ui/form";
-import {
     Command,
     CommandEmpty,
     CommandGroup,
@@ -30,6 +21,11 @@ import { Input } from "@/components/ui/input";
 import { Checkbox } from "@/components/ui/checkbox";
 import { FormEventHandler } from "react";
 import { Card, CardContent } from "./ui/card";
+import { Label } from "./ui/label";
+import { FormControl, FormDescription, FormMessage } from "./ui/form";
+import InputError from "./InputError";
+import { error } from "console";
+import { BackButton } from "./BackButton";
 
 export function FormCreateLearningPath({
     className,
@@ -40,7 +36,7 @@ export function FormCreateLearningPath({
      *
      */
     const { data, setData, post, processing, errors, reset } = useForm({
-        academy: "",
+        collegeExam: "",
         city: "",
         subject: [],
     });
@@ -54,214 +50,112 @@ export function FormCreateLearningPath({
     const academys = [
         {
             label: "Fatec - Faculdade de Tecnologia",
-            value: "fa",
+            value: "fatec",
         },
     ];
 
     return (
-        <Card>
-            <CardContent>
-                <form
-                    onSubmit={submit}
-                    className="space-y-8 max-w-3xl mx-auto py-10"
-                >
-                    <FormField
-                        name="name_8713553238"
-                        render={({ field }) => (
-                            <FormItem className="flex flex-col">
-                                <FormLabel>Faculdade</FormLabel>
-                                <Popover>
-                                    <PopoverTrigger asChild>
-                                        <FormControl>
-                                            <Button
-                                                variant="outline"
-                                                role="combobox"
-                                                className={cn(
-                                                    "w-[200px] justify-between",
-                                                    !field.value &&
-                                                        "text-muted-foreground"
-                                                )}
-                                            >
-                                                {field.value
-                                                    ? academys.find(
-                                                          (acad) =>
-                                                              acad.value ===
-                                                              field.value
-                                                      )?.label
-                                                    : "Select language"}
-                                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                                            </Button>
-                                        </FormControl>
-                                    </PopoverTrigger>
-                                    <PopoverContent className="w-[200px] p-0">
-                                        <Command>
-                                            <CommandInput placeholder="Search language..." />
-                                            <CommandList>
-                                                <CommandEmpty>
-                                                    No language found.
-                                                </CommandEmpty>
-                                                <CommandGroup>
-                                                    {academys.map((acad) => (
-                                                        <CommandItem
-                                                            value={acad.label}
-                                                            key={acad.value}
-                                                            onSelect={() => {}}
-                                                        >
-                                                            <Check
-                                                                className={cn(
-                                                                    "mr-2 h-4 w-4",
-                                                                    acad.value ===
-                                                                        field.value
-                                                                        ? "opacity-100"
-                                                                        : "opacity-0"
-                                                                )}
-                                                            />
-                                                            {acad.label}
-                                                        </CommandItem>
-                                                    ))}
-                                                </CommandGroup>
-                                            </CommandList>
-                                        </Command>
-                                    </PopoverContent>
-                                </Popover>
-                                <FormDescription>
-                                    Escolha a faculdade que você pretende se
-                                    dedicar aos estudos
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-                    <FormField
-                        name="name_7375121613"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Cidade</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder="Ex: São Paulo"
-                                        type=""
-                                        {...field}
-                                    />
-                                </FormControl>
-
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-
-                    <div className="grid grid-cols-12 gap-4">
-                        <div className="col-span-4">
-                            <FormField
-                                name="name_3571387322"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <div className="space-y-1 leading-none">
-                                            <FormLabel>Matematica</FormLabel>
-
-                                            <FormMessage />
-                                        </div>
-                                    </FormItem>
-                                )}
-                            />
+        <section>
+            <BackButton page={"dashboard"}/>
+            <Card>
+                <CardContent>
+                    <form
+                        onSubmit={submit}
+                        className="space-y-8 max-w-3xl mx-auto py-10"
+                    >
+                        <div className="grid gap-6">
+                            <Label>Faculdade</Label>
+                            <Popover>
+                                <PopoverTrigger asChild>
+                                    <Button
+                                        variant="outline"
+                                        role="combobox"
+                                        className={cn(
+                                            "w-[200px] justify-between",
+                                            !data.collegeExam &&
+                                                "text-muted-foreground"
+                                        )}
+                                    >
+                                        {data.collegeExam
+                                            ? academys.find(
+                                                  (acad) =>
+                                                      acad.value ===
+                                                      data.collegeExam
+                                              )?.label
+                                            : "Selecione o vestibular"}
+                                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                    </Button>
+                                </PopoverTrigger>
+                                <PopoverContent className="w-[200px] p-0">
+                                    <Command>
+                                        <CommandInput placeholder="Pesquisar Vestibular" />
+                                        <CommandList>
+                                            <CommandEmpty>
+                                                Vestibular não encontrado
+                                            </CommandEmpty>
+                                            <CommandGroup>
+                                                {academys.map((acad) => (
+                                                    <CommandItem
+                                                        value={acad.label}
+                                                        key={acad.value}
+                                                        onSelect={() => {
+                                                            setData(
+                                                                "collegeExam",
+                                                                acad.value
+                                                            );
+                                                        }}
+                                                    >
+                                                        <Check
+                                                            className={cn(
+                                                                "mr-2 h-4 w-4",
+                                                                acad.value ===
+                                                                    data.collegeExam
+                                                                    ? "opacity-100"
+                                                                    : "opacity-0"
+                                                            )}
+                                                        />
+                                                        {acad.label}
+                                                    </CommandItem>
+                                                ))}
+                                            </CommandGroup>
+                                        </CommandList>
+                                    </Command>
+                                </PopoverContent>
+                            </Popover>
                         </div>
 
-                        <div className="col-span-4">
-                            <FormField
-                                name="name_4598619804"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <div className="space-y-1 leading-none">
-                                            <FormLabel>Português</FormLabel>
+                        {/* <FormDescription>
+                        Escolha a faculdade que você pretende se dedicar aos
+                        estudos
+                    </FormDescription> */}
 
-                                            <FormMessage />
-                                        </div>
-                                    </FormItem>
-                                )}
-                            />
+                        <Label>Cidade</Label>
+                        <Input
+                            placeholder="Ex: São Paulo"
+                            type=""
+                            value={data.city}
+                            onChange={(e) => {
+                                setData("city", e.target.value);
+                            }}
+                        />
+                        <InputError message={errors.city} />
+
+                        <div className="grid grid-cols-12 gap-4">
+                            <div className="col-span-4">
+                                <Checkbox
+                                    checked={data.subject[0]}
+                                    // onCheckedChange={setData("subject")}
+                                />
+
+                                <div className="space-y-1 leading-none">
+                                    <Label>Matematica</Label>
+                                </div>
+                            </div>
                         </div>
-
-                        <div className="col-span-4">
-                            <FormField
-                                name="name_8967988399"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <div className="space-y-1 leading-none">
-                                            <FormLabel>Teste</FormLabel>
-
-                                            <FormMessage />
-                                        </div>
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="grid grid-cols-12 gap-4">
-                        <div className="col-span-6">
-                            <FormField
-                                name="name_3352954929"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <div className="space-y-1 leading-none">
-                                            <FormLabel>teste</FormLabel>
-
-                                            <FormMessage />
-                                        </div>
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-
-                        <div className="col-span-6">
-                            <FormField
-                                name="name_9025514239"
-                                render={({ field }) => (
-                                    <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
-                                        <FormControl>
-                                            <Checkbox
-                                                checked={field.value}
-                                                onCheckedChange={field.onChange}
-                                            />
-                                        </FormControl>
-                                        <div className="space-y-1 leading-none">
-                                            <FormLabel>teste</FormLabel>
-
-                                            <FormMessage />
-                                        </div>
-                                    </FormItem>
-                                )}
-                            />
-                        </div>
-                    </div>
-                    <Button type="submit">Submit</Button>
-                </form>
-            </CardContent>
-        </Card>
+                        <Button type="submit">Submit</Button>
+                    </form>
+                </CardContent>
+            </Card>
+        </section>
     );
 }
